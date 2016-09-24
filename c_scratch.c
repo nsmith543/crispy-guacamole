2e4ef70125a40b83fe167e448103e6584dacd057
nsmith543 <nsmith6@gmail.com>
2016-09-24 09:12:38 -0500

    add c_scratch to repo

---
+++ c_scratch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int trick_cool = 0;
+	char charvar = 'a';
+	trick_cool = 6 + printf("Hello World\n");
+
+	printf("trick_cool value is %d\n", trick_cool);
+	printf("printing %c character and a %d\n", charvar, trick_cool);
+
+	return 0;
+
+}
